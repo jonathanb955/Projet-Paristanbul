a24576722cc2843cc1619137afdc101e2201e648
JBAO <baojonathan95@gmail.com>
2025-05-19 16:48:52 +0200

    creation pade la ge index

---
+++ index.php
@@ -0,0 +1,128 @@
+<?php
+
+use bdd\Bdd;
+
+
+session_start();
+
+if (isset($_GET['logout'])) {
+    session_destroy();
+    header('Location: index.php');
+    exit;
+}
+
+$connecte = isset($_SESSION['connexion']) && $_SESSION['connexion'] === true;
+
+if ($connecte && isset($_SESSION['utilisateur']) && isset($_SESSION['utilisateur']['role'])) {
+    $_SESSION['role'] = $_SESSION['utilisateur']['role'];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paristanbul</title>
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="web site icon" type="png" href="https://previews.123rf.com/images/jovanas/jovanas1602/jovanas160201149/52031915-logo-avi%C3%B3n-volando-alrededor-del-planeta-tierra-azul.jpg">
+    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Libre+Baskerville:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+<header>
+    <div class="d-flex justify-content-center align-items-center position-relative ">
+        <div class="btn-group position-absolute end-0 me-3">
+            <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="bi bi-person-square"></i>
+            </button>
+            <ul class="dropdown-menu text-center">
+                <?php if ($connecte): ?>
+                    <span class="dropdown-item-text"><strong>Bienvenue</strong><br><?php echo $_SESSION['prenom'] . ' ' . $_SESSION['nom']; ?></span>
+                    <li><hr class="dropdown-divider"></li>
+
+                    <?php if ($_SESSION['role'] === 'admin'): ?>
+                        <li><a class="dropdown-item" href="../Projet-Vol/vue/pageAdmin.php"><i class="bi bi-shield-lock-fill"></i> Espace Admin</a></li>
+                    <?php else: ?>
+                        <li><a class="dropdown-item" href="../Projet-Vol/vue/espaceClient.php"><i class="bi bi-person-circle"></i> Espace Client</a></li>
+                    <?php endif; ?>
+
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="?logout=true"><i class="bi bi-box-arrow-right"></i> Déconnexion</a></li>
+                <?php else: ?>
+                    <li><a class="dropdown-item" href="vue/pageConnexion.php">Connexion <i class="bi bi-person-bounding-box"></i></a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="vue/pageInscription.php">Inscription <i class="bi bi-person-plus-fill"></i></a></li>
+                <?php endif; ?>
+            </ul>
+
+        </div>
+
+        <h1 class="mb-0" style="text-transform: capitalize">Paristanbul</h1>
+    </div>
+
+    <nav>
+        <ul>
+            <li><a href="#accueil">Accueil</a></li>
+            <li><a href="#catalogue">Catalogue</a></li>
+            <li><a href="#reseauxsociaux">Nous suivre</a></li>
+
+
+        </ul>
+    </nav>
+</header>
+
+<section id="accueil">
+
+
+    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="5000">
+                <img src="assets/img/maxresdefault.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption custom-caption" style="font-family: 'Times New Roman',serif">
+                    <h1>Le supermarché Paristanbul vous souhaite la bienvenue</h1>
+                    <p>Nous sommes ravis de vous accueillir.<br>
+                        Découvrez nos produits issus du monde entier ainsi que nos services proposés</p>
+                </div>
+            </div>
+
+            <div class="carousel-item" data-bs-interval="5000">
+                <img src="assets/img/youtube-video-gif (1).gif" class="d-block w-100" alt="...">
+                <div class="carousel-caption custom-caption " style="font-family: 'Times New Roman',serif">
+                    <h2><u style="text-transform: capitalize">Découvrez nos produits</u></h2>
+                    <p>Découvrez les saveurs du monde entier grâce à nos magasins <br></p>
+                </div>
+            </div>
+
+            <div class="carousel-item" data-bs-interval="5000">
+                <img src="assets/img/okBcAn1F1F0y0E5DIoED9afIQeadgzXYFkXuvR.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption custom-text" style="font-family: 'Times New Roman',serif">
+                    <h2><u>Couronné Meilleur Aéroport</u></h2>
+                    <p>En 2024, JBSkyTravel élu Meilleur Aéroport pour son excellence et ses services inégalés, offrant une expérience unique et reconnue par tous pour son service de qualité et son innovation.</p>
+                </div>
+            </div>
+        </div>
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <br>
+</section>
